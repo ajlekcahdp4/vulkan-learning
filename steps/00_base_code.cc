1383a2f00f65a58c5d8cacd9ce5a4f90f5b989ab
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-09-26 22:09:03 +0300

    code structure

--- steps/00_base_code.cc
+++ steps/00_base_code.cc
@@ -1,10 +1,35 @@
-#include <GLFW/glfw3.h>
-#include <vulkan/vulkan.hpp>
-
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
 class hello_triangle_application
 {
+  public:
+    void run ()
+    {
+        init_vulkan ();
+        main_loop ();
+        cleanup ();
+    }
+
+  private:
+    void init_vulkan () {}
+
+    void main_loop () {}
+
+    void cleanup () {}
+};
+
+int main ()
+{
+    hello_triangle_application app;
+
+    try
+    {
+        app.run ();
+    } catch ( const std::exception &e )
+    {
+        std::cerr << e.what () << std::endl;
+        return EXIT_FAILURE;
+    }
 }
