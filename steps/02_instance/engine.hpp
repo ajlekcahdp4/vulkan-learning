43b46a83ae6030e12d0ce6813e2edb6d8e4d042d
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-10-05 19:59:27 +0300

    refactored, no loop any more

---
+++ steps/02_instance/engine.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "instance.hpp"
+
+#include <vulkan/vulkan_raii.hpp>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+namespace graphics
+{
+
+struct engine
+{
+  public:
+    engine ()
+    {
+#if !defined(NDEBUG)
+        std::cout << "Making a graphics engine..." << std::endl;
+#endif
+        build_glfw_window ();
+        instance = vkinit::make_instance ("first instance");
+    }
+
+    ~engine ()
+    {
+#if !defined(NDEBUG)
+        std::cout << "Destroing graphicd engine..." << std::endl;
+#endif
+        glfwTerminate ();
+    }
+
+  private:
+    // glfw window parameters
+    uint32_t width              = 800;
+    uint32_t height             = 600;
+    GLFWwindow *window          = nullptr;
+    vk::raii::Instance instance = nullptr;
+
+    // glfw setup
+    void build_glfw_window ()
+    {
+        glfwInit ();
+        glfwInitHint (GLFW_CLIENT_API, GLFW_NO_API);   // no default rendering client
+        // resizing breaks the swapchain, we'll disable it for now
+        glfwInitHint (GLFW_RESIZABLE, GLFW_FALSE);
+        if ( window = glfwCreateWindow (width, height, "First window", nullptr, nullptr) )
+        {
+#if !defined(NDEBUG)
+            std::cout << "Successfully made a GLFW window" << std::endl;
+#endif
+        }
+        else
+        {
+#if !defined(NDEBUG)
+            std::cout << "GLFW window creation failed" << std::endl;
+#endif
+        }
+    }
+};
+}   // namespace graphics
---
+++ steps/02_instance/instance.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+namespace graphics
+{
+namespace vkinit
+{
+vk::raii::Instance make_instance (const std::string &appName)
+{
+#if !defined(NDEBUG)
+    std::cout << "Making an vulkan instance..." << std::endl;
+#endif
+
+    vk::raii::Context context;
+
+    uint32_t version = context.enumerateInstanceVersion ();
+#if !defined(NDEBUG)
+    std::cout << "System can support vulkan Variant: " << VK_API_VERSION_VARIANT (version)
+              << ", Major: " << VK_API_VERSION_MAJOR (version)
+              << ", Minor: " << VK_API_VERSION_MINOR (version)
+              << ", Patch: " << VK_API_VERSION_PATCH (version) << std::endl;
+#endif
+    vk::ApplicationInfo appInfo {appName.c_str (), version, "First engine", version, version};
+
+    /*
+     * Everything with Vulkan is "opt-in", so we need to query which extensions glfw needs
+     * in order to interface with vulkan.
+     */
+    uint32_t glfw_ext_count = 0;
+    const char **arr_glfw_extensions;
+    arr_glfw_extensions = glfwGetRequiredInstanceExtensions (&glfw_ext_count);
+
+    std::vector<const char *> glfw_extensions (arr_glfw_extensions,
+                                               arr_glfw_extensions + glfw_ext_count);
+
+#if !defined(NDEBUG)
+    std::cout << "Extensions to be requated by GLFW:" << std::endl;
+    for ( auto &extension : glfw_extensions )
+        std::cout << "\t\"" << extension << "\"" << std::endl;
+#endif
+
+    vk::InstanceCreateInfo create_info {
+        vk::InstanceCreateFlags {}, &appInfo, 0, nullptr, glfw_ext_count, glfw_extensions.data ()};
+
+    try
+    {
+        return vk::raii::Instance {context, create_info};
+    } catch ( vk::SystemError &err )
+    {
+#if !defined(NDEBUG)
+        std::cout << "Failed to create instance!" << std::endl;
+#endif
+        return nullptr;
+    }
+}
+
+}   // namespace vkinit
+}   // namespace graphics
